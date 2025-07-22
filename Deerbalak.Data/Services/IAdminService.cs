439a2e44556684217ea5e6a10edfd7ed81410076
Y********* <admin@test.domains.com>
2025-07-22 07:01:01 +0300

    Add Deerbalak.Data project manually

---
+++ Deerbalak.Data/Services/IAdminService.cs
@@ -0,0 +1,16 @@
+﻿using Deerbalak.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deerbalak.Data.Services
+{
+    public interface IAdminService
+    {
+        Task<List<Post>> GetReportedPostsAsync();
+        Task ApproveReportAsync(int postId);
+        Task RejectReportAsync(int postId);
+    }
+}
